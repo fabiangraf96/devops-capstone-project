cee29150b036c3177263f52749dd509523f3a06f
fabiangraf96 <fabian.graf@siemens.com>
2022-11-16 01:40:05 -0500

    Changes to pass Lint Check

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -71,7 +72,7 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
- 
+
 
 ######################################################################
 # READ AN ACCOUNT
@@ -81,13 +82,11 @@ def get_accounts(account_id):
     """
     Reads an Account
     This endpoint will read an Account based the account_id that is requested
-     """
+    """
     app.logger.info("Request to read an Account with id: %s", account_id)
-        
     account = Account.find(account_id)
     if not account:
-       abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-        
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -103,7 +102,7 @@ def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-       abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
